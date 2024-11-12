46578738b647dbcfea8e5d9064bb5efd4880d1a7
register <2503440295@qq.com>
2024-11-12 19:27:25 +0800

    修改了excel的策略

--- Strategy/Read/ExcelReadStrategy.cs
+++ Strategy/Read/ExcelReadStrategy.cs
@@ -1,3 +1,6 @@
+using System.Data;
+using System.IO;
+using Excel;
 using UnityEngine;
 
 namespace ExcelConvert.Controller
@@ -6,19 +9,39 @@ namespace ExcelConvert.Controller
     {
         public void ReadData(string path, out string modelName, out string containerModelName)
         {
-            modelName = CreateMoel();
-            containerModelName = CreateContainerModel();
-            Debug.Log($"{path}从读取了Excel文件");
+            modelName = "";
+            containerModelName = "";
+            using (FileStream fs = new FileStream(path,FileMode.Open,FileAccess.Read))
+            {
+                IExcelDataReader excelReader = ExcelReaderFactory.CreateBinaryReader(fs);
+                DataSet result = excelReader.AsDataSet();
+                if (result.Tables.Count < 0)
+                {
+                    Debug.Log($"Excel文件为空");
+                    return;
+                }
+                
+                //读取文件中所有的表
+                for (int i = 0; i < result.Tables.Count; i++)
+                {
+                    DataTable table = result.Tables[i];
+                    for (int j = 0; j < table.Rows.Count; j++)
+                    {
+                        DataRow row = table.Rows[i];
+                        for (int k = 0; k < table.Columns.Count; k++)
+                        {
+                            Debug.Log(row[k]);
+                        }
+                    }
+                    
+                }
+                
+                Debug.Log($"从{path}读取了Excel文件");    
+            }
+            
         }
 
-        public string CreateMoel()
-        {
-            return "Model";
-        }
-
-        public string CreateContainerModel()
-        {
-            return "ContainerModel";
-        }
+  
+       
     }
 }
--- Strategy/Read/IReadStrategy.cs
+++ Strategy/Read/IReadStrategy.cs
@@ -3,8 +3,7 @@ namespace ExcelConvert.Controller
     public interface IReadStrategy
     {
         public void ReadData(string path,out string modelName, out string containerModelName);
-        public string CreateMoel();
-        public string CreateContainerModel();
+     
     }
     
 }
